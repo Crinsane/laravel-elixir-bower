3a330cfe0a4319675bb4407bf1fe166b0136b967
Thomas GERVAIS <thomas.gervais@gmail.com>
2015-05-15 21:31:19 +0200

    Add image support

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ var minify = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var changed = require('gulp-changed');
+var base64 = require('gulp-base64');
+var test = require('gulp-if');
+var ignore = require('gulp-ignore');
+var getFileSize = require("filesize");
+
 var _ = require('lodash');
 
 elixir.extend('bower', function(options) {
@@ -25,10 +30,15 @@ elixir.extend('bower', function(options) {
         },
         font: {
             output: 'public/fonts'
+        },
+        img: {
+            output: 'public/imgs',
+            extInline: [ 'gif', 'png'],
+            maxInlineSize: 32 * 1024 //max 32k on ie8
         }
     }, options);
 
-    gulp.task('bower', ['bower-css', 'bower-js', 'bower-fonts']);
+    gulp.task('bower', ['bower-css', 'bower-js', 'bower-fonts', 'bower-imgs']);
 
     gulp.task('bower-css', function () {
         var onError = function (err) {
@@ -45,6 +55,11 @@ elixir.extend('bower', function(options) {
         return gulp.src(mainBowerFiles({debugging: options.debugging}))
             .on('error', onError)
             .pipe(filter('**/*.css'))
+            .pipe(test(options.img.maxInlineSize > 0, base64({
+                extensions: options.img.extInline,
+                maxImageSize: options.img.maxInlineSize, // bytes 
+                debug: options.debugging,
+            })))
             .pipe(concat(options.css.file))
             .pipe(minify())
             .pipe(gulp.dest(options.css.output))
@@ -112,6 +127,50 @@ elixir.extend('bower', function(options) {
                 icon: __dirname + '/../icons/laravel.png',
                 message: ' '
             }));
+    });
+
+    gulp.task('bower-imgs', function () {
+
+        var onError = function (err) {
+
+            notify.onError({
+                title: "Laravel Elixir",
+                subtitle: "Bower Files Images Copy Failed!",
+                message: "Error: <%= error.message %>",
+                icon: __dirname + '/../icons/fail.png'
+            })(err);
+
+            this.emit('end');
+        };
+
+        var isInline = function (file) {
+            
+            var filesize = file.stat ? getFileSize(file.stat.size) : getFileSize(Buffer.byteLength(String(file.contents)));
+            var fileext = file.path.split('.').pop();
+            
+            if (options.debugging)
+            {
+                console.log("Size of file:" + file.path + " (" + 1024*parseFloat(filesize) +" / max="+options.img.maxInlineSize+")");
+            }
+            
+            return options.img.extInline.indexOf(fileext) > -1 && 1024*parseFloat(filesize) < options.img.maxInlineSize;
+        }
+
+        return gulp.src(mainBowerFiles({
+            debugging: options.debugging,
+            filter: (/\.(png|bmp|gif|jpg|jpeg)$/i)
+            }))
+            .on('error', onError)
+            .pipe(ignore.exclude(isInline)) // Exclude inlined images
+            .pipe(changed(options.img.output))
+            .pipe(gulp.dest(options.img.output))
+            .pipe(notify({
+                title: 'Laravel Elixir',
+                subtitle: 'Images Bower Files Imported!',
+                icon: __dirname + '/../icons/laravel.png',
+                message: ' '
+            }));
+
     });
     
 
